95e040d12712daa8f1ae24b9675be9964ae43fc7
shi-hao <shi7631470@163.com>
2019-02-21 10:36:17 +0800

    add

--- install.sh
+++ install.sh
@@ -39,21 +39,44 @@ echo "install the neocomplete.vim-ver.2.1 done!"
 #add the vim config 
 #check the file .vimrc in home path, if not exist, create it
 targetFile="$HOME/.vimrc"
-vim_config="./vimrc"
+my_config="./vimrc"
 if [ ! -f $targetFile ];then
-	cat  $vim_config  >>  $targetFile 
+	cat  $my_config  >>  $targetFile 
 	echo "create the $targetFile done"
 else
 	#backup .vimrc
 	timestamp=$(date +%s)
-	file_backup="$HOME/.vimrc$timestamp"
+	file_backup="$targetFile$timestamp"
 	mv  $targetFile  $file_backup
 
 	#delete the old config content
 	config_start="vim config by shi706734862@163.com start"
 	config_end="vim config by shi706734862@163.com end"
 	sed  "/$config_start/,/$config_end/d" $file_backup > $targetFile
-	cat  $vim_config  >>  $targetFile 
+	cat  $my_config  >>  $targetFile 
 
 	echo "add the vim config done!"
+fi
+
+###############################################################
+#add shell command config
+#check the file .bashrc in home path, if not exists, create it
+targetFile="$HOME/.bashrc"
+my_config="./bashrc"
+if [ ! -f $targetFile ];then
+	cat  $my_config  >>  $targetFile 
+	echo "create the $targetFile done"
+else
+	#backup .bashrc
+	timestamp=$(date +%s)
+	file_backup="$targetFile$timestamp"
+	mv  $targetFile  $file_backup
+
+	#delete the old config content
+	config_start="bash config by shi706734862@163.com start"
+	config_end="bash config by shi706734862@163.com end"
+	sed  "/$config_start/,/$config_end/d" $file_backup > $targetFile
+	cat  $my_config  >>  $targetFile 
+
+	echo "add the bash config done!"
 fi
